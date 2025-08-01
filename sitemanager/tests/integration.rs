bcc8cc926ed3ea02d51cec119a6a3175a68f8268
Jak Bracegirdle <jak@bracegirdle.io>
2025-08-01 15:15:40 +0100

    Added debugging prints to tests

--- sitemanager/tests/integration.rs
+++ sitemanager/tests/integration.rs
@@ -1,11 +1,13 @@
 use dotenvy::dotenv;
 use std::env;
+use sitemanager::endpoints::*;
 
 #[tokio::test]
 async fn test_list_sites() {
     dotenv().ok();
     let api_key = env::var("SITEMANAGER_API_KEY").expect("SITEMANAGER_API_KEY must be set");
-    let result = sitemanager::endpoints::list_sites::list_sites(&api_key).await;
+    let result = list_sites::list_sites(&api_key).await;
+    println!("{:?}", result);
     assert!(result.is_ok());
 }
 
@@ -13,6 +15,16 @@ async fn test_list_sites() {
 async fn test_list_sd_wan_configs() {
     dotenv().ok();
     let api_key = env::var("SITEMANAGER_API_KEY").expect("SITEMANAGER_API_KEY must be set");
-    let result = sitemanager::endpoints::list_sd_wan_configs::list_sd_wan_configs(&api_key).await;
+    let result = list_sd_wan_configs::list_sd_wan_configs(&api_key).await;
+    println!("{:?}", result);
+    assert!(result.is_ok());
+}
+
+#[tokio::test]
+async fn test_get_isp_metrics() {
+    dotenv().ok();
+    let api_key = env::var("SITEMANAGER_API_KEY").expect("SITEMANAGER_API_KEY must be set");
+    let result = get_isp_metrics::get_isp_metrics(&api_key, "5m", None, None, Option::from("24h")).await;
+    println!("{:?}", result);
     assert!(result.is_ok());
 }
